c6d3ada54c84460be95b844fcdf60321d5473f4f
ruochen <wangrui@ruochen.email>
2022-03-04 16:56:08 +0800

    普通数据类型注入

--- spring_ioc_study/src/main/java/com/ruochen/dao/impl/UserDaoImpl.java
+++ spring_ioc_study/src/main/java/com/ruochen/dao/impl/UserDaoImpl.java
@@ -3,6 +3,19 @@ package com.ruochen.dao.impl;
 import com.ruochen.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
+
+    private String username;
+    private int age;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /*
     public UserDaoImpl() {
         System.out.println("UserDaoImpl创建...");
     }
@@ -14,9 +27,11 @@ public class UserDaoImpl implements UserDao {
     public void destroy() {
         System.out.println("销毁方法...");
     }
+    */
 
     @Override
     public void save() {
+        System.out.println(username + "-------" + age);
         System.out.println("save running...");
     }
 }
--- spring_ioc_study/src/main/java/com/ruochen/service/impl/UserServiceImpl.java
+++ spring_ioc_study/src/main/java/com/ruochen/service/impl/UserServiceImpl.java
@@ -7,10 +7,20 @@ public class UserServiceImpl implements UserService {
 
     private UserDao userDao;
 
+    // 构造方式注入
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public UserServiceImpl() {
+    }
+
     // set 方式注入
+    /*
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
+    */
 
     @Override
     public void save() {
