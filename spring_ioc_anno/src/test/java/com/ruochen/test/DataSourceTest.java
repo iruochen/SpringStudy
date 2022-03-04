cc590d743fef3a70db77bedfba003432e1be63fb
ruochen <wangrui@ruochen.email>
2022-03-04 20:00:42 +0800

    抽取jdbc.properties配置文件

--- spring_ioc_anno/src/test/java/com/ruochen/test/DataSourceTest.java
+++ spring_ioc_anno/src/test/java/com/ruochen/test/DataSourceTest.java
@@ -5,13 +5,35 @@ import com.alibaba.druid.pool.DruidPooledConnection;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.junit.Test;
 
-import java.beans.PropertyVetoException;
 import java.sql.Connection;
+import java.util.ResourceBundle;
 
 public class DataSourceTest {
     @Test
+    // 测试手动创建 c3p0 数据源(加载properties配置文件)
+    public void test3() throws Exception {
+        // 读取配置文件
+        ResourceBundle rb = ResourceBundle.getBundle("jdbc");
+        String driver = rb.getString("jdbc.driver");
+        String url = rb.getString("jdbc.url");
+        String username = rb.getString("jdbc.username");
+        String password = rb.getString("jdbc.password");
+        // 创建数据源对象
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        // 设置数据源的基本连接数据
+        dataSource.setDriverClass(driver);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+
+    @Test
     // 测试手动创建 druid 数据源
-    public void test2() throws Exception{
+    public void test2() throws Exception {
         // 创建数据源对象
         DruidDataSource dataSource = new DruidDataSource();
         // 设置数据源的基本连接数据
