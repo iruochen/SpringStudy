1ea9c3b754ee7e87c262db64301eeea18502a6fa
ruochen <wangrui@ruochen.email>
2022-03-04 15:27:15 +0800

    bean 生命周期配置

--- spring_ioc_study/src/main/java/com/ruochen/dao/impl/UserDaoImpl.java
+++ spring_ioc_study/src/main/java/com/ruochen/dao/impl/UserDaoImpl.java
@@ -7,6 +7,14 @@ public class UserDaoImpl implements UserDao {
         System.out.println("UserDaoImpl创建...");
     }
 
+    public void init() {
+        System.out.println("初始化方法...");
+    }
+
+    public void destroy() {
+        System.out.println("销毁方法...");
+    }
+
     @Override
     public void save() {
         System.out.println("save running...");
--- spring_ioc_study/src/test/java/com/ruochen/test/SpringTest.java
+++ spring_ioc_study/src/test/java/com/ruochen/test/SpringTest.java
@@ -16,9 +16,7 @@ public class SpringTest {
     public void test1() {
         ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserDao userDao1 = (UserDao) app.getBean("userDao");
-        UserDao userDao2 = (UserDao) app.getBean("userDao");
         System.out.println(userDao1);
-        System.out.println(userDao2);
-
+        ((ClassPathXmlApplicationContext) app).close();
     }
 }
