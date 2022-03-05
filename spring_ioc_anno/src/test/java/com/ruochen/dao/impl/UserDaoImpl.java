0cca2f1e78bc57776a0903f356d3cd410e88ca7d
ruochen <wangrui@ruochen.email>
2022-03-05 13:41:44 +0800

    get 方式注入，xml配置方式

---
+++ spring_ioc_anno/src/test/java/com/ruochen/dao/impl/UserDaoImpl.java
@@ -0,0 +1,10 @@
+package com.ruochen.dao.impl;
+
+import com.ruochen.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public void save() {
+        System.out.println("save running....");
+    }
+}
---
+++ spring_ioc_anno/src/test/java/com/ruochen/web/UserController.java
@@ -0,0 +1,14 @@
+package com.ruochen.web;
+
+import com.ruochen.service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserController {
+
+    public static void main(String[] args) {
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService userService = app.getBean(UserService.class);
+        userService.save();
+    }
+}
