8ddc691ac226ccc6f305bc2c36139824a0fba82f
ruochen <wangrui@ruochen.email>
2022-03-05 13:49:52 +0800

    æ³è¯驻ä注解方式实现注入

--- spring_ioc_anno/src/test/java/com/ruochen/dao/impl/UserDaoImpl.java
+++ spring_ioc_anno/src/test/java/com/ruochen/dao/impl/UserDaoImpl.java
@@ -1,7 +1,11 @@
 package com.ruochen.dao.impl;
 
 import com.ruochen.dao.UserDao;
+import org.springframework.stereotype.Component;
 
+// <bean id="userDao" class="com.ruochen.dao.impl.UserDaoImpl"></bean>
+// 注解方式配置
+@Component("userDao")
 public class UserDaoImpl implements UserDao {
     @Override
     public void save() {
--- spring_ioc_anno/src/test/java/com/ruochen/service/impl/UserServiceImpl.java
+++ spring_ioc_anno/src/test/java/com/ruochen/service/impl/UserServiceImpl.java
@@ -2,8 +2,17 @@ package com.ruochen.service.impl;
 
 import com.ruochen.dao.UserDao;
 import com.ruochen.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+// <bean id="userService" class="com.ruochen.service.impl.UserServiceImpl">
+@Component("userService")
 public class UserServiceImpl implements UserService {
+    // <property name="userDao" ref="userDao"></property>
+    // 注解方式：userDao 注入到 userService
+    @Autowired
+    @Qualifier("userDao")
     private UserDao userDao;
 
     public void setUserDao(UserDao userDao) {
