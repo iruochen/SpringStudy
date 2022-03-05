da4eecaa4e5314301b739dc53171935004d913b8
ruochen <wangrui@ruochen.email>
2022-03-05 13:59:21 +0800

    åSpring注解开发ï -- æ¼原始注解

--- spring_ioc_anno/src/test/java/com/ruochen/dao/impl/UserDaoImpl.java
+++ spring_ioc_anno/src/test/java/com/ruochen/dao/impl/UserDaoImpl.java
@@ -2,10 +2,12 @@ package com.ruochen.dao.impl;
 
 import com.ruochen.dao.UserDao;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 // <bean id="userDao" class="com.ruochen.dao.impl.UserDaoImpl"></bean>
 // 注解方式配置
-@Component("userDao")
+// @Component("userDao")
+@Repository("userDao")  // 效果与 Component 一样，可读性强
 public class UserDaoImpl implements UserDao {
     @Override
     public void save() {
--- spring_ioc_anno/src/test/java/com/ruochen/service/impl/UserServiceImpl.java
+++ spring_ioc_anno/src/test/java/com/ruochen/service/impl/UserServiceImpl.java
@@ -5,19 +5,27 @@ import com.ruochen.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 // <bean id="userService" class="com.ruochen.service.impl.UserServiceImpl">
-@Component("userService")
+// @Component("userService")
+@Service("userService")  // 效果与 Component 一样，可读性强
 public class UserServiceImpl implements UserService {
     // <property name="userDao" ref="userDao"></property>
     // 注解方式：userDao 注入到 userService
-    @Autowired
-    @Qualifier("userDao")
+    // @Autowired  // 按照数据类型从 Spring 容器中进行匹配
+    // @Qualifier("userDao")  // 按照id值从容器中进行匹配，但是注意：@Qualifier 要结合 @Autowired 一起使用
+    @Resource(name = "userDao")  // @Resource 相当于 @Autowired+@Qualifier
     private UserDao userDao;
 
+    // 使用注解 set 方法可以删除
+    /*
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
+    */
 
     @Override
     public void save() {
