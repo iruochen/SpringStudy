bf844b73cc3c634973965e8498362c75089a24df
ruochen <wangrui@ruochen.email>
2022-03-07 16:53:07 +0800

    Spring数据响应æ -页面跳转-返回ModelAndView形式1

--- spring_mvc/note/SpringMVC.md
+++ spring_mvc/note/SpringMVC.md
@@ -19,3 +19,12 @@
 - ViewResolver 解析后返回具体 View
 - DispatcherServlet 根据 View 进行渲染视图（即将模型数据填充至视图中）。DispatcherServlet 响应用户
 
+## SpringMVC 数据响应方式
+- 页面跳转
+  - 直接返回字符串：此种方式会将返回的字符串与视图解析器的前后缀拼接后跳转
+  - 通过 ModelAndView 对象返回
+- 回写数据
+  - 直接返回字符串
+  - 返回对象或集合
+
+
--- spring_mvc/src/main/java/com/ruochen/controller/UserController.java
+++ spring_mvc/src/main/java/com/ruochen/controller/UserController.java
@@ -3,10 +3,25 @@ package com.ruochen.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+    @RequestMapping(value = "/quick2")
+    public ModelAndView save2() {
+        /*
+            Model：模型 作用封装数据
+            View： 视图 作用展示数据
+         */
+        ModelAndView modelAndView = new ModelAndView();
+        // 设置模型数据
+        modelAndView.addObject("username", "ruochen");
+        // 设置视图名称
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
+
     // 请求地址 http://localhost:8080/user/quick
     @RequestMapping(value = "/quick", method = RequestMethod.GET, params = {"username"})
     public String save() {
