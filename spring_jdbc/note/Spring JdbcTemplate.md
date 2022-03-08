1ea97a7414413ad07c943717f6244259499c601f
ruochen <wangrui@ruochen.email>
2022-03-08 20:44:27 +0800

    Spring JdbcTemplate开发步骤

---
+++ spring_jdbc/note/Spring JdbcTemplate.md
@@ -0,0 +1,5 @@
+## JdbcTemplate 开发步骤
+- 导入 spring-jdbc 和 spring-tx 坐标
+- 创建数据库和实体
+- 创建 JdbcTemplate 对象
+- 执行数据库操作
---
+++ spring_jdbc/src/test/java/com/ruochen/test/JdbcTemplateTest.java
@@ -0,0 +1,27 @@
+package com.ruochen.test;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.beans.PropertyVetoException;
+
+public class JdbcTemplateTest {
+    @Test
+    // 测试JdbcTemplate开发步骤
+    public void Test1() throws PropertyVetoException {
+        // 创建数据库对象
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql:///test");
+        dataSource.setUser("root");
+        dataSource.setPassword("ruochen666");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        // 设置数据源对象 知道数据库在哪儿
+        jdbcTemplate.setDataSource(dataSource);
+        // 执行操作
+        int row = jdbcTemplate.update("insert into account values (?,?)", "tom", 5000);
+        System.out.println(row);
+    }
+}
