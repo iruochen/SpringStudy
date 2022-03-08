62b50182a47e1d0a54f2614833d9d41490a4696a
ruochen <wangrui@ruochen.email>
2022-03-08 20:55:14 +0800

    Spring产生JdbcTemplate对象

--- spring_jdbc/src/test/java/com/ruochen/test/JdbcTemplateTest.java
+++ spring_jdbc/src/test/java/com/ruochen/test/JdbcTemplateTest.java
@@ -2,11 +2,22 @@ package com.ruochen.test;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.beans.PropertyVetoException;
 
 public class JdbcTemplateTest {
+    @Test
+    // 测试Spring 产生 JdbcTemplate 对象
+    public void Test2() throws PropertyVetoException {
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JdbcTemplate jdbcTemplate = app.getBean(JdbcTemplate.class);
+        int row = jdbcTemplate.update("insert into account values (?,?)", "zhangsan", 5000);
+        System.out.println(row);
+    }
+
     @Test
     // 测试JdbcTemplate开发步骤
     public void Test1() throws PropertyVetoException {
