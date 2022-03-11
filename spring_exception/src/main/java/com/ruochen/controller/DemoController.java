f7496774d3a222aa07674c9fa3630817f9b2a737
ruochen <wangrui@ruochen.email>
2022-03-12 07:44:01 +0800

    SpringMVC异常处理-简单映射一æ异常处理器

--- spring_exception/src/main/java/com/ruochen/controller/DemoController.java
+++ spring_exception/src/main/java/com/ruochen/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.ruochen.controller;
 
+import com.ruochen.exception.MyException;
 import com.ruochen.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,13 +15,13 @@ public class DemoController {
     private DemoService demoService;
 
     @RequestMapping("/show")
-    public String show(@RequestParam(value = "name", required = true)String name) throws FileNotFoundException {
+    public String show() throws MyException {
         System.out.println("show running....");
-        demoService.show1();
+        // demoService.show1();
         // demoService.show2();
         // demoService.show3();
         // demoService.show4();
-        // demoService.show5();
+        demoService.show5();
         return "index";
     }
 
---
+++ spring_exception/src/main/webapp/error.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ruochen
+  Date: 2022/3/12
+  Time: 7:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head>
+        <title>Title</title>
+    </head>
+    <body>
+        <h1>通用错误提示页面</h1>
+    </body>
+</html>
