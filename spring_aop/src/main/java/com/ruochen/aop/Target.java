ce9527a7fa89e1e1facf6e2b6b91d146bcdcb64b
ruochen <wangrui@ruochen.email>
2022-03-12 09:49:39 +0800

    xml方式实现aop-通知的种类

--- spring_aop/src/main/java/com/ruochen/aop/Target.java
+++ spring_aop/src/main/java/com/ruochen/aop/Target.java
@@ -3,6 +3,7 @@ package com.ruochen.aop;
 public class Target implements TargetInterface {
     @Override
     public void save() {
+        int i = 1 / 0;
         System.out.println("save running....");
     }
 }
