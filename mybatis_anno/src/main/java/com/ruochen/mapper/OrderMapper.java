ef57cd187e70ad47815af10b5daa4561cefd9b34
ruochen <wangrui@ruochen.email>
2022-03-12 20:38:57 +0800

    mybatis注解开发--一对一注解开å方式二

--- mybatis_anno/src/main/java/com/ruochen/mapper/OrderMapper.java
+++ mybatis_anno/src/main/java/com/ruochen/mapper/OrderMapper.java
@@ -1,6 +1,8 @@
 package com.ruochen.mapper;
 
 import com.ruochen.domain.Order;
+import com.ruochen.domain.User;
+import org.apache.ibatis.annotations.One;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
@@ -9,6 +11,7 @@ import java.util.List;
 
 public interface OrderMapper {
 
+    /*
     @Select("select *,o.id oid from orders o, user u where o.uid=u.id")
     @Results({
             @Result(column = "oid", property = "id"),
@@ -17,6 +20,22 @@ public interface OrderMapper {
             @Result(column = "uid", property = "user.id"),
             @Result(column = "username", property = "user.username"),
             @Result(column = "password", property = "user.password")
+    })
+    List<Order> findAll();
+    */
+
+    @Select("select * from orders")
+    @Results({
+            @Result(column = "id", property = "id"),
+            @Result(column = "ordertime", property = "ordertime"),
+            @Result(column = "total", property = "total"),
+            @Result(
+                    property = "user",  // 要封装的属性名称
+                    column = "uid",  // 根据哪个字段去查询 user 表数据
+                    javaType = User.class,  // 要封装的实体类型
+                    // select属性：代表查询哪个接口的方法获得数据
+                    one = @One(select = "com.ruochen.mapper.UserMapper.findById")
+            )
     })
     List<Order> findAll();
 }
