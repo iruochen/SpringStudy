1787aa24cdb548ae8adb393aa1c6278aa3545d8f
ruochen <wangrui@ruochen.email>
2022-03-12 14:16:01 +0800

    spring事务控制-基于注解的声明式事务控制

--- spring_tx/src/main/java/com/ruochen/dao/impl/AccountDaoImpl.java
+++ spring_tx/src/main/java/com/ruochen/dao/impl/AccountDaoImpl.java
@@ -1,14 +1,14 @@
 package com.ruochen.dao.impl;
 
 import com.ruochen.dao.AccountDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository("accountDao")
 public class AccountDaoImpl implements AccountDao {
+    @Autowired
     private JdbcTemplate jdbcTemplate;
-
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
     @Override
     public void out(String outMan, double money) {
--- spring_tx/src/main/java/com/ruochen/service/impl/AccountServiceImpl.java
+++ spring_tx/src/main/java/com/ruochen/service/impl/AccountServiceImpl.java
@@ -2,15 +2,21 @@ package com.ruochen.service.impl;
 
 import com.ruochen.dao.AccountDao;
 import com.ruochen.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service("accountService")
+@Transactional(isolation = Isolation.REPEATABLE_READ)
 public class AccountServiceImpl implements AccountService {
+    @Autowired
     private AccountDao accountDao;
 
-    public void setAccountDao(AccountDao accountDao) {
-        this.accountDao = accountDao;
-    }
-
     @Override
+    // 就近原则
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
     public void transfer(String outMan, String inMan, double money) {
         accountDao.out(outMan, money);
         int i = 1 / 0;
