7bfd5302150722d15c71f839b382c10f8705df34
ruochen <wangrui@ruochen.email>
2022-03-12 22:09:07 +0800

    ssm整合-原始整合方式-逻辑代码编写

--- ssm/src/main/java/com/ruochen/controller/AccountController.java
+++ ssm/src/main/java/com/ruochen/controller/AccountController.java
@@ -1,17 +1,36 @@
 package com.ruochen.controller;
 
 import com.ruochen.domain.Account;
+import com.ruochen.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
+@Controller
+@RequestMapping("/account")
 public class AccountController {
 
+    @Autowired
+    private AccountService accountService;
+
     // 保存
+    @RequestMapping("/save")
+    @ResponseBody
     public String save(Account account) {
-        return null;
+        accountService.save(account);
+        return "保存成功";
     }
 
     // 查询
     public ModelAndView findAll() {
-        return null;
+        List<Account> accountList = accountService.findAll();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("accountList", accountList);
+        modelAndView.setViewName("accountList");
+        return modelAndView;
     }
 }
