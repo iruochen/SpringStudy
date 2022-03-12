07d91ffa04b714484b49e827a9650e01e3fb4d63
ruochen <wangrui@ruochen.email>
2022-03-12 09:53:52 +0800

    xml方式实现aop-切点表达式的抽取

--- spring_aop/src/main/java/com/ruochen/aop/Target.java
+++ spring_aop/src/main/java/com/ruochen/aop/Target.java
@@ -3,7 +3,7 @@ package com.ruochen.aop;
 public class Target implements TargetInterface {
     @Override
     public void save() {
-        int i = 1 / 0;
+        // int i = 1 / 0;
         System.out.println("save running....");
     }
 }
